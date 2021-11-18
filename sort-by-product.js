7833cbd7243099dd178e6d5285854bf3e0d846cc
Robert Lewis <robsim37@gmail.com>
2021-11-18 10:30:37 -0500

    cleaner

--- sort-by-product.js
+++ sort-by-product.js
@@ -1,8 +1,7 @@
 const sortByProduct=arr=>{
     const products = [];
-    arr.forEach((e,i)=>products.push(`${e*(i+1)}:${i}`));
-    return products.sort((a,b)=>parseInt(a.split(':')[0])-parseInt(b.split(':')[0]))
-                    .map(e=>parseInt(e.split(':')[0])/(parseInt(e.split(':')[1])+1))
+    arr.forEach((e,i)=>products.push({i, value: e*(i+1)}));
+    return products.sort((a,b)=>a.value-b.value).map(e=>e.value/(e.i+1))
 }
 
 console.log(sortByProduct([23, 2, 3, 4, 5]).join(', '));
