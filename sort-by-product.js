00eed7d962f66a5f0feebe0f5578e1b2c02297e9
Robert Lewis <robsim37@gmail.com>
2021-11-18 10:18:11 -0500

    bug fix

--- sort-by-product.js
+++ sort-by-product.js
@@ -1,3 +1,10 @@
-const sortByProduct=arr=>arr.map((v,i)=>v*(i+1)).sort((a,b)=>a-b);
+const sortByProduct=arr=>{
+    const products = [];
+    arr.forEach((e,i)=>products.push(`${e*(i+1)}:${i}`));
+    return products.sort((a,b)=>parseInt(a.split(':')[0])-parseInt(b.split(':')[0]))
+                    .map(e=>parseInt(e.split(':')[0])/(parseInt(e.split(':')[1])+1))
+}
 
 console.log(sortByProduct([23, 2, 3, 4, 5]).join(', '));
+console.log(sortByProduct([10, 12, 14, 16, 18]).join(', '));
+console.log(sortByProduct([11, 9, 7, 5, 5, 3, 1, 5]).join(', '));
